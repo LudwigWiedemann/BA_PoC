b64f07d9a57d9f1ab4f656f8f9745d2763676d91
wiedemannlu <wiedemannlu@cip.ifi.lmu.de>
2025-11-25 19:31:11 +0100

    added admin permissions to github-deploy-user

--- 0.0.1/frontend/poc_frontend/src/LoginView.jsx
+++ 0.0.1/frontend/poc_frontend/src/LoginView.jsx
@@ -53,7 +53,7 @@ export default function LoginView() {
 
     return (
         <div style={{ maxWidth: 400, margin: '5rem auto', textAlign: 'center' }}>
-            <h1>IdP PoC test</h1>
+            <h1>IdP PoC</h1>
             <h2 style={{ minHeight: '5rem', visibility: greeting ? 'visible' : 'hidden' }}>
                 {greeting}
             </h2>
