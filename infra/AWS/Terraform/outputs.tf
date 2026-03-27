67aeca396edb5c729ca21584ebd38f54cdac2d56
wiedemannlu <wiedemannlu@cip.ifi.lmu.de>
2026-03-28 00:24:18 +0100

    resolve duplicate definition of frontend_bucket in outputs.tf

--- infra/AWS/Terraform/outputs.tf
+++ infra/AWS/Terraform/outputs.tf
@@ -2,7 +2,7 @@ output "eks_cluster_name" {
   value = module.eks.cluster_name
 }
 
-output "frontend_bucket_name" {
+output "aws_frontend_bucket_name" {
   value = aws_s3_bucket.frontend.bucket
 }
 
