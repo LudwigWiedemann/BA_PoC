1e2f8563a703ddafe766641a72d1569f6ffb19ee
wiedemannlu <wiedemannlu@cip.ifi.lmu.de>
2026-03-28 00:03:53 +0100

    insanely big commit to automate the full deploymen process of the baseline

--- infra/AWS/Terraform/outputs.tf
+++ infra/AWS/Terraform/outputs.tf
@@ -5,3 +5,8 @@ output "eks_cluster_name" {
 output "frontend_bucket_name" {
   value = aws_s3_bucket.frontend.bucket
 }
+
+output "ingress_hostname" {
+  description = "Hostname des AWS Load Balancers"
+  value       = data.kubernetes_service.nginx_ingress.status[0].load_balancer[0].ingress[0].hostname
+}
--- infra/GCP/Terraform/outputs.tf
+++ infra/GCP/Terraform/outputs.tf
@@ -1,7 +1,3 @@
-output "gke_endpoint" {
-  value     = google_container_cluster.gke.endpoint
-  sensitive = true
-}
 
 output "gke_cluster_name" {
   value = google_container_cluster.gke.name
@@ -9,4 +5,10 @@ output "gke_cluster_name" {
 
 output "frontend_bucket_name" {
   value = google_storage_bucket.frontend.name
+}
+
+output "ingress_ip" {
+  description = "Externe IP des NGINX Ingress Load Balancers"
+  value       = data.kubernetes_service.nginx_ingress.status[0].load_balancer[0].ingress[0].ip
+  sensitive   = false
 }
