8bdd55830f9191c7cdd4859bd9efb6a45cc2c87b
wiedemannlu <wiedemannlu@cip.ifi.lmu.de>
2026-03-28 16:03:31 +0100

    add backend.tf to store tfstate persistently

---
+++ infra/AWS/Terraform/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "azure-poc-terraform-state-065875800629"
+    key            = "aws/eks/terraform.tfstate"
+    region         = "eu-central-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+}
