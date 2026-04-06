eda64d9b83bd89b2c4e3e3bc787a1efee42c16b0
wiedemannlu <wiedemannlu@cip.ifi.lmu.de>
2026-04-06 22:21:16 +0200

    use outputs correctly (i hope) for bucket name

--- infra/AWS/Terraform/outputs.tf
+++ infra/AWS/Terraform/outputs.tf
@@ -10,3 +10,11 @@ output "ingress_hostname" {
   description = "Hostname des AWS Load Balancers"
   value       = data.kubernetes_service.nginx_ingress.status[0].load_balancer[0].ingress[0].hostname
 }
+
+output "wg_gateway_public_ip" {
+  value = aws_instance.wg_gateway.public_ip
+}
+
+output "wg_gateway_private_ip" {
+  value = aws_instance.wg_gateway.private_ip
+}
--- infra/GCP/Terraform/gcp-outputs.tf
+++ infra/GCP/Terraform/gcp-outputs.tf
@@ -12,3 +12,11 @@ output "ingress_ip" {
   value       = data.kubernetes_service.nginx_ingress.status[0].load_balancer[0].ingress[0].ip
   sensitive   = false
 }
+
+output "wg_gateway_public_ip" {
+  value = google_compute_instance.wg_gateway.network_interface[0].access_config[0].nat_ip
+}
+
+output "wg_gateway_private_ip" {
+  value = google_compute_instance.wg_gateway.network_interface[0].network_ip
+}
